f13e23e7c8f61b89ea63b38a8c17f6f863f05b3c
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2024-12-12 18:46:56 +0700

    feat: update logo and metatags

--- src/vene-app-frontend/src/components/footer.jsx
+++ src/vene-app-frontend/src/components/footer.jsx
@@ -3,11 +3,15 @@ import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-6 w-full mt-12">
+    <footer className="bg-primary text-black py-6 w-full mt-12">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <h3 className="text-lg font-semibold">VENE</h3>
+            <img
+              alt="logo"
+              className="w-[100px] h-auto mb-3"
+              src="/vene-black.png"
+            />
             <p className="text-xs mt-1">© 2024 All rights reserved.</p>
           </div>
           {/* Add social media icons */}
--- src/vene-app-frontend/src/components/navbar.jsx
+++ src/vene-app-frontend/src/components/navbar.jsx
@@ -1,7 +1,16 @@
 import { Search } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    const searchValue = e.target[0].value;
+
+    navigate(`/search?q=${searchValue}`);
+  };
+
   return (
     <header className="sticky">
       <div className="container p-[0.875rem] md:px-0 m-auto w-full flex justify-between items-center">
@@ -9,16 +18,19 @@ const Navbar = () => {
           <Link to="/">
             <img
               alt="logo"
-              className="w-[167px] h-[40px]"
-              src="/logo-vene.png"
+              className="w-[120px] h-auto"
+              src="/vene-black.png"
             />
           </Link>
           <div className="flex items-center rounded-xl gap-3 px-4 py-3 bg-[#F0F0F0]">
             <Search className="w-5 h-5 stroke-muted-foreground" />
-            <input
-              placeholder="Find amazing event"
-              className="focus:outline-none bg-transparent text-sm w-64"
-            />
+            <form onSubmit={onSearch}>
+              <input
+                type="search"
+                placeholder="Find amazing event"
+                className="focus:outline-none bg-transparent text-sm w-64"
+              />
+            </form>
           </div>
         </div>
         <div className="flex gap-8">
