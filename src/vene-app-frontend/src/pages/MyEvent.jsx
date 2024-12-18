12d0587a1fbbfff6f8fc357481d2312e239b5b3e
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2024-12-18 11:54:43 +0700

    fix: my event style

--- src/vene-app-frontend/src/pages/MyEvent.jsx
+++ src/vene-app-frontend/src/pages/MyEvent.jsx
@@ -33,7 +33,7 @@ const MyEvent = () => {
 
   return (
     <div className="min-h-screen p-8">
-      <div className="max-w-4xl mx-auto">
+      <div className="container mx-auto">
         <header className="flex justify-between items-center mb-8">
           <h1 className="text-4xl font-bold">Events</h1>
           <div className="flex gap-4">
